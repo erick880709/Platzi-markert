cb5233440aa555c101d45fbc890aa27f42b966d0
Erick880709 <erick880709@hotmail.com>
2022-10-12 08:07:57 -0500

    actualizacion de la libreria
    actualizacion de la libreria

---
+++ platzi-markert/src/main/java/com/platzi/markert/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.platzi.markert;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Saludar")
+public class HolaMundoController {
+
+    @GetMapping("/Hola")
+    public String Saludar(){
+        return ("Hola mundo es mi primer programa");
+    }
+}
