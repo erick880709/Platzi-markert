141de85a18edc3aeb9c4f3368a59631bb6e16378
Erick880709 <erick880709@hotmail.com>
2022-10-14 16:01:10 -0500

    crud repository
    crud repository

--- platzi-markert/src/main/java/com/platzi/markert/persistence/ProductoRepository.java
+++ platzi-markert/src/main/java/com/platzi/markert/persistence/ProductoRepository.java
@@ -2,14 +2,36 @@ package com.platzi.markert.persistence;
 
 import com.platzi.markert.persistence.crud.ProductoCrudRepository;
 import com.platzi.markert.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    public List<Producto> getAllByCategoria(int idCategoria){
+        return productoCrudRepository.getByIdCategoria(idCategoria);
+    }
+
+    public Optional<List<Producto>> escasos (int cantidadStock, boolean estado){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, estado);
+    }
+
+    public Optional<Producto> getProductoById (int id){
+        return productoCrudRepository.findById(id);
+    }
+
+    public Producto save (Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete (int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
--- platzi-markert/src/main/java/com/platzi/markert/persistence/crud/ProductoCrudRepository.java
+++ platzi-markert/src/main/java/com/platzi/markert/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,15 @@
 package com.platzi.markert.persistence.crud;
 
 import com.platzi.markert.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository <Producto,Integer>{
+    List<Producto> findByIdCategoria(int idCategoria);
+    @Query(value="SELECT * from where id_categoria=?", nativeQuery = true)
+    List<Producto> getByIdCategoria(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado (int cantidadStock , boolean estado);
 }
