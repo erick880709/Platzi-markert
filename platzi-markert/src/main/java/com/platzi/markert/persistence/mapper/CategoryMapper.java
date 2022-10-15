47d2e64d4b39054895da029eaf3c5f12ec0d039e
Erick880709 <erick880709@hotmail.com>
2022-10-15 13:41:28 -0500

    category Mapper
    category Mapper

---
+++ platzi-markert/src/main/java/com/platzi/markert/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.platzi.markert.persistence.mapper;
+
+import com.platzi.markert.domain.Category;
+import com.platzi.markert.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source="idCategoria",target="categoryId"),
+            @Mapping(source="descripcion",target="category"),
+            @Mapping(source="estado",target="active")
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos",ignore = true)
+    Categoria toCategoria (Category category);
+}
